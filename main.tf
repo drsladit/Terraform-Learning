ff7ca4c3f755b87e1b8b59cf483d2e85f58cc0b0
drsladit <drsladit.91@gmail.com>
2021-07-10 19:52:51 +0530

    Added complex variable list map set examples

--- main.tf
+++ main.tf
@@ -1,14 +1,54 @@
+# string type
 variable "mystr" {
 	type = string
 	default = "welcome to terraform"
 }
 
+# number type
 variable "myint" {
 	type = number
 	default = "999"
 }
 
+# Bool type
 variable "mybol" {
 	type = bool
 	default = false
 }
+
+# List type 1: Not specifing type
+variable "mylist" {
+	type = list
+	default = [1,2,3]
+}
+
+# List type 2: Specifing type
+variable "mylist2" {
+	type = list(string)
+	default = ["a","b","c"]
+}
+
+# map type 1: without type specified
+variable "mymap" {
+	type = map
+	default = {
+	"key1" = "value1"
+	}
+}
+
+# map type 2: with type specified
+variable "mymap2" {
+	type = map(number)
+	default = {
+	"key1" = 1, "key2" = 2
+	}
+}
+
+
+# set type
+variable "myset" {
+	type = set(number)
+	default = [1,2,3,4]
+}
+
+
