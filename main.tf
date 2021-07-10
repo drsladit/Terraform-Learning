af7ce7e89c012205e674a77a0f73f29eb604381d
drsladit <drsladit.91@gmail.com>
2021-07-10 18:39:51 +0530

    Added basic variable type to terraform file

---
+++ main.tf
@@ -0,0 +1,14 @@
+variable "mystr" {
+	type = string
+	default = "welcome to terraform"
+}
+
+variable "myint" {
+	type = number
+	default = "999"
+}
+
+variable "mybol" {
+	type = bool
+	default = false
+}
